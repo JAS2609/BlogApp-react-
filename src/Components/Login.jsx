c08cfc82f863f075b96b8acf714e6914352f0647
jas <jas72609@gmail.com>
2025-07-01 16:14:34 +0530

    text color change

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -40,7 +40,7 @@ console.log("Dispatched login");
                         <Logo width="100%" />
                     </span>
         </div>
-        <h2 className="text-center text-2xl font-bold leading-tight">Sign in to your account</h2>
+        <h2 className="text-center text-2xl font-bold leading-tight text-black">Sign in to your account</h2>
         <p className="mt-2 text-center text-base text-black/60">
                     Don&apos;t have any account?&nbsp;
                     <Link
